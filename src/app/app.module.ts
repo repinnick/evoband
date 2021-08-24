b3f31ca0305dfa9f4e757db699ba593a5970041a
repinnick <mikalai.repin@gmail.com>
2021-08-24 22:05:35 +0300

    feat: create background, header (start)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { GalaxyComponent } from './components/galaxy/galaxy.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    GalaxyComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/galaxy/galaxy.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GalaxyComponent } from './galaxy.component';
+
+describe('GalaxyComponent', () => {
+  let component: GalaxyComponent;
+  let fixture: ComponentFixture<GalaxyComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GalaxyComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GalaxyComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
