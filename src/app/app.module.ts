af365b9d9280a49dce1eef9fd5d784f4cdb6c841
repinnick <mikalai.repin@gmail.com>
2021-08-26 23:05:38 +0300

    feat: create pages component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,22 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { GalaxyComponent } from './components/galaxy/galaxy.component';
 import { HamburgerToggleDirective } from './directives/hamburger-toggle.directive';
+import { HomeComponent } from './pages/home/home.component';
+import { PagesRoutingModule } from './pages/pages-routing.module';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     GalaxyComponent,
-    HamburgerToggleDirective
+    HamburgerToggleDirective,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
+    PagesRoutingModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/components/galaxy/galaxy.component.ts
+++ src/app/components/galaxy/galaxy.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-galaxy',
   templateUrl: './galaxy.component.html',
   styleUrls: ['./galaxy.component.scss']
 })
-export class GalaxyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class GalaxyComponent {}
