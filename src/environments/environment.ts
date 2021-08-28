c6b52b49de6392f23e67d6f4e528269434660a53
Mikalai Repin <check.point9917@yandex.ru>
2021-08-28 21:41:46 +0300

    feat: firebase deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCBKeA9Hk2lUZx_uhaArj3VpwOcvdseS-Q",
+    authDomain: "evoband-app.firebaseapp.com",
+    projectId: "evoband-app",
+    storageBucket: "evoband-app.appspot.com",
+    messagingSenderId: "709874925815",
+    appId: "1:709874925815:web:23bcfce17c7eb66c89d3f2"
+  }
 };
 
 /*
