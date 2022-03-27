423adacf23c557dbeb10fcf7e4a7eb5a364c1c42
repinnick <check.point9917@yandex.ru>
2022-03-27 21:02:39 +0300

    fix: change background color for header

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,17 +1,24 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'evoband-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class HeaderComponent implements OnInit{
   public isBurgerMenu = false;
   public isActive = false;
+  public isScrollEvent = false;
 
   @HostListener('window:resize', ['$event'])
   private onResize(event: any): void {
     this.isBurgerMenu = event.target.innerWidth <= 860;
+  }
+
+  @HostListener('window:scroll', [])
+  private onScroll(): void {
+    this.isScrollEvent = window.innerHeight < window.pageYOffset + 78;
   }
 
   ngOnInit(): void {
